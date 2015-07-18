d04f7fbc1ec4ad309b1df5da6931bfb2957c5559
cateia97 <bristolkr@gmail.com>
2015-07-17 22:06:07 -0400

    routing changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def after_sign_in_path_for(user)
+    users_path
+  end
+
+  def after_sign_out_path_for(user)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
+
   root to: 'visitors#index'
+
   devise_for :users
+
   resources :users
+
+  resources :posts do
+  	resources :comments, :only => [:create, :destroy]
+  end
+
 end
